da571e0535f331a06be9d6686c69cdafa35eb44e
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-01-28 15:07:54 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,6 +3,6 @@ def call(Map stageParams) {
 checkout([
 	$class: 'GitSCM',
 	branches: [[name: stageParams.branch]],
-	userRemoteConfigs: [[credentialsId: 'bitbucket_gaurav', url: stageParams.url]]
+	userRemoteConfigs: [[credentialsId: stageParams.credentials, url: stageParams.url]]
 	])
 }
