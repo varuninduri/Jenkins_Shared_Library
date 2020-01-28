014c7e41d6532806ae7c6742610bde65eca4998d
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-01-28 17:46:01 +0530

    Update mavenTasks.groovy

--- vars/mavenTasks.groovy
+++ vars/mavenTasks.groovy
@@ -2,4 +2,22 @@ def call(Map config) {
 if(config.step == "clean"){
 	sh 'mvn clean'
 	}
+else if(config.step == "compile"){
+	sh 'mvn compile'
+	}
+else if(config.step == "sonar"){
+	sh 'mvn sonar:sonar'
+	}
+else if(config.step == "test"){
+	sh 'mvn test'
+	}
+else if(config.step == "coverage"){
+	sh 'mvn cobertura:cobertura'
+	}
+else if(config.step == "package"){
+	sh 'mvn package'
+	}
+else{
+	echo "please configure maven steps"
+	}	
 }
