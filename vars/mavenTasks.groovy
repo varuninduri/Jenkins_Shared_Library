0366ed9c04e3b88c732497f06fedc2abf2d8d715
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-01-29 14:53:08 +0530

    Update mavenTasks.groovy

--- vars/mavenTasks.groovy
+++ vars/mavenTasks.groovy
@@ -22,7 +22,7 @@ else if(config.step == "coverage"){
 	}
 	}
 else if(config.step == "package"){
-	sh 'mvn --settings /home/indurivarun/artifactorysettings.xml package'
+	sh 'mvn --settings /root/artifactorysettings.xml package'
 	}
 else{
 	echo "please configure maven steps"
