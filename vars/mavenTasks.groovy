f584dabbcc0d132b0758bf42f57398cd2683083d
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-01-29 11:42:36 +0530

    Update mavenTasks.groovy

--- vars/mavenTasks.groovy
+++ vars/mavenTasks.groovy
@@ -1,4 +1,4 @@
-def call(Map config, boolean abc = false) {
+def call(Map config, boolean check = false) {
 if(config.step == "clean"){
 	sh 'mvn clean'
 	}
@@ -11,18 +11,26 @@ else if(config.step == "sonar"){
 else if(config.step == "test"){
 	sh 'mvn test'
 	
-	if(config.testreport!=abc){
+	if(config.testreport!=check){
 	junit 'target/surefire-reports/*.xml'	
 	}
 	}
 else if(config.step == "coverage"){
 	sh 'mvn cobertura:cobertura'
-	if(config.coveragereport!=abc){
+	if(config.coveragereport!=check){
 	cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: 'target/site/cobertura/coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
 	}
 	}
 else if(config.step == "package"){
+	if(config.typeBuild=="jar"){
 	sh 'mvn package'
+	}
+	else if(config.typeBuild=="war"){
+	sh 'mvn war:war'
+	}
+	else{
+	echo "please define type of Build with param typeBuild"
+	}
 	}
 else{
 	echo "please configure maven steps"
