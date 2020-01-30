743f7a29ef6321f92ff7b48a8b3430af3449e57f
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-01-30 18:33:37 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,4 +1,18 @@
+import hudson.plugins.git.GitTool
+import org.gs.stpl.java.Git
+import org.gs.stpl.util.Utilities
+import org.gs.stpl.java.model.JavaPipelineInfo
 
+Utilities utilities = new Utilities(this)
+		String scmType = getScmType(scm)
+		Git git = new Git(this)
+		git.git = tool name: 'GIT', type: 'hudson.plugins.git.GitTool' //TODO: Tool name should be in properties file and match install tool name.
+JavaPipelineInfo javaPipelineInfo = new JavaPipelineInfo(this, utilities, scmType)
+		javaPipelineInfo.isMainlineBuild = utilities.isMainLineBranch(scmType, env.BRANCH_NAME)
+	stage('Checkout') {
+			checkout scm
+		}
+/*
 def call(Map stageParams) {
 checkout([
 	$class: 'GitSCM',
@@ -6,3 +20,4 @@ checkout([
 	userRemoteConfigs: [[credentialsId: stageParams.credentials, url: stageParams.url]]
 	])
 }
+*/
