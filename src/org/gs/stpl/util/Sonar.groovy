289d1ca8302171cb47204693ae69391d7474cb9f
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-03 18:21:12 +0530

    Create Sonar.groovy

---
+++ src/org/gs/stpl/util/Sonar.groovy
@@ -0,0 +1,25 @@
+package org.gs.stpl.util
+
+/**
+ * Class for Maven specific utilities that can be used by all pipelines.
+ */
+class Sonar implements Serializable {
+
+    String sonarTool
+    String qualityGate
+
+    /**
+     * Creates a new Maven utility class.
+     *
+     * @param steps the pipeline dsl context
+     * @param mavenTool string of the mavenTool (retrieved via the 'tool' dsl command)
+     * @param jdkTool string of the jdkTool (retrieved via the 'tool' dsl command)
+     */
+    Sonar(String sonarTool, String qualityGate) {
+        assert SonarTool: 'I need sonarTool to be valid'
+        assert qualityGate: 'I need qualityGate to be valid'
+
+        this.sonarTool = mavenTool
+        this.qualityGate = qualityGate
+    }
+    }
