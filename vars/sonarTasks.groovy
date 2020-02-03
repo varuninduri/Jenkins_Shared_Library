eddd8333e2856ad15c91688adf6d2b8e68159f8c
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-03 18:03:52 +0530

    Update sonarTasks.groovy

--- vars/sonarTasks.groovy
+++ vars/sonarTasks.groovy
@@ -1,10 +1,10 @@
-def call(Map config) {
+def call(Map config, Map quality) {
     println (qualitygate instanceof boolean)
     println qualitygate
     withSonarQubeEnv('sonar') {
         sh 'mvn sonar:sonar'
     } // SonarQube taskId is automatically attached to the pipeline context
-  if (qualitygate=="true"){
+  if (quality.qualitygate=="true"){
 stage("Quality Gate"){
     timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
     def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
