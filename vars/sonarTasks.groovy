7b37410d53d3c50227b5bcde00f8d1504d3b0593
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-04 11:09:09 +0530

    Update sonarTasks.groovy

--- vars/sonarTasks.groovy
+++ vars/sonarTasks.groovy
@@ -3,7 +3,5 @@ import org.gs.stpl.util.Sonar
 def call(String sonarTool, boolean qualityGate) {
  Sonar sonar = new Sonar (sonarTool, qualityGate)
  SonarQube sonarqube
- sonarTool = sonar.sonarTool
- qualityGate = sonar.qualityGate
   sonarqube.sonarAnalysis(sonar)  
 }
