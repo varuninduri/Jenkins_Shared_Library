656e9a0e189acef605026f5e1788ea75e8a5d8be
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-04 11:03:42 +0530

    Update Sonar.groovy

--- src/org/gs/stpl/util/Sonar.groovy
+++ src/org/gs/stpl/util/Sonar.groovy
@@ -15,11 +15,11 @@ class Sonar implements Serializable {
      * @param mavenTool string of the mavenTool (retrieved via the 'tool' dsl command)
      * @param jdkTool string of the jdkTool (retrieved via the 'tool' dsl command)
      */
-    Sonar(String sonarTool, String qualityGate) {
+    Sonar(String sonarTool, boolean qualityGate) {
         assert SonarTool: 'I need sonarTool to be valid'
         assert qualityGate: 'I need qualityGate to be valid'
 
-        this.sonarTool = mavenTool
+        this.sonarTool = sonarTool
         this.qualityGate = qualityGate
     }
     }
