f6bcf3a802ed7c82e40c6da50334a881d6fae8c7
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-04 11:32:27 +0530

    Update sonarTasks.groovy

--- vars/sonarTasks.groovy
+++ vars/sonarTasks.groovy
@@ -1,9 +1,9 @@
 import org.gs.stpl.java.SonarQube
 import org.gs.stpl.util.Sonar
-def call(LinkedHashMap<> map) {
- println (map.get(qualityGate))
- println (map.get(sonarTool))
- println (map.get(qualityGate).getClass())
+def call(String sonarTool, boolean qualityGate) {
+ println (qualityGate)
+ println (sonarTool)
+ println (qualityGate.getClass())
  /*Sonar sonar = new Sonar (map.get(sonarTool), map.get(qualityGate))
  SonarQube sonarqube
   sonarqube.sonarAnalysis(sonar)*/  
