029237fdbeb552928aa2276d56f15baa3b325f19
varuninduri <45063946+varuninduri@users.noreply.github.com>
2020-02-13 14:29:19 +0530

    Update npmTasks.groovy

--- vars/npmTasks.groovy
+++ vars/npmTasks.groovy
@@ -1,5 +1,7 @@
 def call(Map config, boolean check = false) {
 if(config.step == "install"){
-	sh 'sudo npm install -g @oracle/ojet-cli'
+	sh 'sudo npm install -g @oracle/ojet-cli oracle/oraclejet-tooling cordova node-gyp'
+	sh 'sudo ojet add sass'
+	sh 'npm install'
 	}
   }
